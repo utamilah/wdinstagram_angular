600c736048969c6f64b5165d0c26098ca6a7df98
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-05-09 22:51:24 -0400

    populate index page

--- js/app.js
+++ js/app.js
@@ -1,53 +1,50 @@
 (function(){
   "use strict";
 
-  let wdinstagramEntry = [
-    {
-      photo_url: 'http://www.designbolts.com/wp-content/uploads/2015/11/Inspiring-Brushpen-Crayola-Hand-Lettering-Examples-by-David-Milan-4.jpg',
-      author: 'utamilah',
-      body: 'Bibendum Tristique Ridiculus Dapibus Venenatis'
-    },
-    {
-      photo_url: 'https://s-media-cache-ak0.pinimg.com/736x/40/67/7f/40677fec7b709578781439f74c6d7c8e.jpg',
-      author: 'utamilah',
-      body: 'Bibendum Tristique Ridiculus Dapibus Venenatis'
-    },
-    {
-      photo_url: 'https://s-media-cache-ak0.pinimg.com/736x/4b/f2/06/4bf20658209e047fa8f83da7f3b534c8.jpg',
-      author: 'utamilah',
-      body: 'Bibendum Tristique Ridiculus Dapibus Venenatis'
-    },
-    {
-      photo_url: 'http://68.media.tumblr.com/2e920673bf0ab959e1d642d5c9c7371e/tumblr_nifdqmWxDB1ryq9ado1_500.jpg',
-      author: 'utamilah',
-      body: 'Bibendum Tristique Ridiculus Dapibus Venenatis'
-    }
-  ]
-
   angular
   .module("wdinstagram", [
-    "ui.router"
+    "ui.router",
+    "ngResource"
   ])
-  .config(["$stateProvider", Router])
-  .controller("WDInstagramController", [
+  .config([
+    "$stateProvider",
+    RouterFunction
+  ])
+  .factory("EntryFactory", [
+    "$resource",
+    EntryFactoryFunction
+  ])
+  .controller("EntryIndexController", [
+    "EntryFactory",
+    EntryIndexControllerFunction
+  ])
+  .controller("EntryShowController", [
+    "EntryFactory",
     "$stateParams",
-    "$state",
-    WDInstagramControllerFunction
+    EntryShowControllerFunction
   ])
 
-  function Router($stateProvider){
-    $stateProvider.state("author", {
-      url: '/:author',
-      controller: 'WDInstagramController',
-      controllerAs: 'vm',
-      templateUrl: 'index.html'
+  function RouterFunction($stateProvider){
+    $stateProvider
+    .state("entryIndex", {
+      url: '/entries',
+      templateUrl: 'js/ng-views/index.html',
+      controller: 'EntryIndexController',
+      controllerAs: 'vm'
     })
   }
 
-  function WDInstagramControllerFunction($stateParams, $state){
-    console.log($state)
-    this.wdinstagrams = wdinstagramEntry
-  }
+function EntryIndexControllerFunction(EntryFactory){
+  this.entries = EntryFactory.query();
+}
+
+function EntryShowControllerFunction(EntryFactory, $stateParams){
+  this.entry = EntryFactory.get({id: $stateParams.id})
+}
+
+function EntryFactoryFunction($resource){
+  return $resource("http://localhost:3000/entries/:id.json")
+}
 
 })();
 
